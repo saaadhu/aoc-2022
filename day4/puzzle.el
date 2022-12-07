2236e518a4ca0f2afdd3c761b47a5fc8174b16aa
Senthil Kumar Selvaraj <senthilkumar.selvaraj@microchip.com>
2022-12-07 15:15:13 +0530

    Improve code [Day 4]

--- day4/puzzle.el
+++ day4/puzzle.el
@@ -10,22 +10,22 @@
     (insert-file-contents name)
     (buffer-string)))
 
-(defun breakout-ranges (lst)
-  "Breakout LST in \"a-\"b \"x-\"y form into (a b x y)."
-  (pcase-let* ((`(, lst0 ,lst1) lst)
+(defun breakout-ranges (line)
+  "Breakout LINE in \"a-b,x-y\" form into (a b x y)."
+  (pcase-let* ((`(, lst0 ,lst1) (split-string line ","))
                 (`(,a,b) (mapcar 'string-to-number (split-string lst0 "-")))
                 (`(,x,y) (mapcar 'string-to-number (split-string lst1 "-"))))
     (list  a b x y)))
 
 (defun overlap-p (lst)
-  "Is range A fully contained in range B?"
+  "For ((a b) (x y)) in LST, does one range overlap another?"
   (pcase-let ((`(,a,b,x,y) (breakout-ranges lst)))
     (or (and (>= a x) (<= a y))
         (and (>= x a) (<= x b)))))
         
 
 (defun fully-contained-p (lst)
-  "Is range A fully contained in range b?"
+  "For ((a b) (x y)) in LST, is one range fully contained in another?"
   (pcase-let ((`(,a,b,x,y) (breakout-ranges lst)))
     (or (and (>= a x) (<= b y))
         (and (>= x a) (<= y b)))))
@@ -33,13 +33,11 @@
 (defun solve-puzzle-1 ()
   "Solve Day4 Puzzle1."
   (let* ((contents (readfile "input"))
-         (lines (split-string contents "\n"))
-         (ranges (mapcar (lambda (line) (split-string line ",")) lines)))
-    (seq-count 'fully-contained-p ranges)))
+         (lines (split-string contents "\n")))
+    (seq-count 'fully-contained-p lines)))
 
 (defun solve-puzzle-2 ()
-  "Solve Day3 Puzzle1."
+  "Solve Day4 Puzzle2."
   (let* ((contents (readfile "input"))
-         (lines (split-string contents "\n"))
-         (ranges (mapcar (lambda (line) (split-string line ",")) lines)))
-    (seq-count 'overlap-p ranges)))
+         (lines (split-string contents "\n")))
+    (seq-count 'overlap-p lines)))
